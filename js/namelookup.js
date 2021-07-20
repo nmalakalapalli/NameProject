19f189ee9d64fab7306561a6bfe0148717ee5a4b
nmalakalapalli <45083667+nmalakalapalli@users.noreply.github.com>
2021-07-20 15:36:41 -0400

    small update to better handle filepaths

--- js/namelookup.js
+++ js/namelookup.js
@@ -1,7 +1,7 @@
 function playName(form){
 	var name = document.getElementById('name').value
 	console.log(name)
-	var audiopath = 'D:/NamePronouncer/recordings/' + name + '-3.m4a';
+	var audiopath = './recordings/' + name + '-3.m4a';
 	const nameaudio = new Audio(audiopath);
 	return nameaudio.play();
 	}
