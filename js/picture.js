06607d60a3c21ed1e18cfe0a868083e19f0a2370
vargopal <77592238+vargopal@users.noreply.github.com>
2021-07-20 15:05:25 -0400

    New Page

--- js/picture.js
+++
@@ -1,5 +0,0 @@
-function picture(){
-	var x = document.getElementByID("SA Logo").src;
-	document.getElementByID("demo").innerHTML = x;
-	}
-//This is trying to get the State Auto Logo onto our page.
