954747a57a8ef19d03117c70e3e5ccc57c8e31d2
nmalakalapalli <45083667+nmalakalapalli@users.noreply.github.com>
2021-07-20 16:26:06 -0400

    radio button functionality added

--- js/namelookup.js
+++ js/namelookup.js
@@ -1,7 +1,17 @@
 function playName(form){
+	//declares variables for the input name and for the voice version selected
 	var name = document.getElementById('name').value
-	console.log(name)
-	var audiopath = './recordings/' + name + '-3.m4a';
+	var vcheck = document.getElementById('voice_1').checked
+	
+	//if voice 1 was chosen, plays Nikhil's recordings, if not, plays Varun's
+	if (vcheck) {
+		avers = '-1.m4a';
+	} else {
+		avers = '-3.m4a';
+	}
+	
+	//calls and plays the corresponding audio recording
+	var audiopath = './recordings/' + name + avers;
 	const nameaudio = new Audio(audiopath);
 	return nameaudio.play();
 	}
