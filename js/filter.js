66c320671c47d4d274fe246b37879ef86780a66f
nmalakalapalli <45083667+nmalakalapalli@users.noreply.github.com>
2021-07-22 09:56:58 -0400

    fixed filter + consolidated table

--- js/filter.js
+++ js/filter.js
@@ -1,13 +1,15 @@
-function myFunction(){
+function tableFilter(){
 		var input, filter, table, tr,td, i,txtValue;
 		input = document.getElementById("myInput");
 		filter = input.value.toUpperCase();
 		table = document.getElementById("myTable");
 		tr = table.getElementsByTagName("tr");
+		console.log('tr');
 		for(i=0; i < tr.length; i++){
 			td =tr[i].getElementsByTagName("td")[0];
 			if(td){
 				txtValue=td.textContent||td.innerText;
+				console.log(txtValue);
 				if(txtValue.toUpperCase().indexOf(filter)> -1){
 					tr[i].style.display="";
 					}else{
