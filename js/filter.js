41138356e5ff8ce85773b740a1038fab8d5d55fc
nmalakalapalli <45083667+nmalakalapalli@users.noreply.github.com>
2021-07-22 10:52:17 -0400

    Update filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,16 +1,20 @@
 function tableFilter(){
 		var input, filter, table, tr,td, i,txtValue;
 		input = document.getElementById("myInput");
+		
+		//filter is the variable defined by the input in uppercase
 		filter = input.value.toUpperCase();
 		table = document.getElementById("myTable");
 		tr = table.getElementsByTagName("tr");
-		console.log('tr');
+		console.log(filter);
+		
 		for(i=0; i < tr.length; i++){
 			td =tr[i].getElementsByTagName("td")[0];
 			if(td){
 				txtValue=td.textContent||td.innerText;
-				console.log(txtValue);
-				if(txtValue.toUpperCase().indexOf(filter)> -1){
+				
+				//if name starts with letters input by the filter, then the table row is output
+				if(txtValue.toUpperCase().startsWith(filter)){
 					tr[i].style.display="";
 					}else{
 					tr[i].style.display = "none"
